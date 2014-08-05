0fa57c0bbbda7e22a93533487c842de8a1e4ddeb
clowwindy <clowwindy42@gmail.com>
2014-08-05 19:02:46 +0800

    remove logs

--- src/chinadns.c
+++ src/chinadns.c
@@ -320,7 +320,6 @@ static int parse_ip_list() {
     free(line);
 
   qsort(ip_list.ips, ip_list.entries, sizeof(struct in_addr), cmp_in_addr);
-  LOG("%d ips loaded\n", ip_list.entries);
   fclose(fp);
   return 0;
 }
