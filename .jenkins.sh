35e6300ee97d462d95b0318330496696c61ec00f
clowwindy <clowwindy42@gmail.com>
2015-02-01 16:45:36 +0800

    test -d and -m

--- .jenkins.sh
+++ .jenkins.sh
@@ -24,14 +24,19 @@ function run_test {
     return 0
 }
 
+rm src/*.gc*
 run_test ./autogen.sh
 run_test ./configure --enable-debug
 run_test make
-run_test tests/test.py tests/google.com
-run_test tests/test.py tests/facebook.com
-run_test tests/test.py tests/twitter.com
+run_test tests/test.py -a '-c chnroute.txt -l iplist.txt' -t tests/google.com
+run_test tests/test.py -a '-c chnroute.txt -l iplist.txt' -t tests/facebook.com
+run_test tests/test.py -a '-c chnroute.txt -l iplist.txt' -t tests/twitter.com
+run_test tests/test.py -a '-d -c chnroute.txt -l iplist.txt' -t tests/twitter.com
+run_test tests/test.py -a '-c chnroute.txt' -t tests/twitter.com
+run_test tests/test.py -a '-m -c chnroute.txt' -t tests/twitter.com
 
 gcov src/*.c
+rm src/*.html
 cd src && gcovr -r . --html  --html-details  -o index.html
 gcovr -r . | grep TOTAL | rev | cut -d' ' -f 1 | rev  > /tmp/chinadns-coverage
 
--- tests/test.py
+++ tests/test.py
@@ -5,31 +5,40 @@ import sys
 import os
 import signal
 import time
+import argparse
 from subprocess import Popen
 
-chinadns = ['src/chinadns', '-l', 'iplist.txt', '-c', 'chnroute.txt',
-            '-p', '15353', '-v']
+parser = argparse.ArgumentParser(description='test ChinaDNS')
+parser.add_argument('-a', '--arguments', type=list, default=[])
+parser.add_argument('-t', '--test-command', type=str, default=None)
+
+config = parser.parse_args()
+
+arguments = config.arguments
+chinadns = ['src/chinadns', '-p', '15353', '-v'] + arguments
 
 p1 = Popen(chinadns, shell=False, bufsize=0, close_fds=True)
+try:
 
-with open(sys.argv[-1]) as f:
-    dig_cmd = f.read()
+    with open(config.test_command) as f:
+        dig_cmd = f.read()
 
-time.sleep(1)
+    time.sleep(1)
 
-p2 = Popen(dig_cmd.split() + ['-p', '15353'], shell=False,
-           bufsize=0, close_fds=True)
+    p2 = Popen(dig_cmd.split() + ['-p', '15353'], shell=False,
+            bufsize=0, close_fds=True)
 
-if p2 is not None:
-    r = p2.wait()
-if r == 0:
-    print 'test passed'
+    if p2 is not None:
+        r = p2.wait()
+    if r == 0:
+        print 'test passed'
 
-for p in [p1]:
-    try:
-        os.kill(p.pid, signal.SIGTERM)
-        os.waitpid(p.pid, 0)
-    except OSError:
-        pass
+    sys.exit(r)
+finally:
+    for p in [p1]:
+        try:
+            os.kill(p.pid, signal.SIGTERM)
+            os.waitpid(p.pid, 0)
+        except OSError:
+            pass
 
-sys.exit(r)
