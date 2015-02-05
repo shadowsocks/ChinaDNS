3d2bc8c2b44e39cc1349e63365a329bbe0f73874
clowwindy <clowwindy42@gmail.com>
2015-02-05 17:07:20 +0800

    test more arguments

--- .jenkins.sh
+++ .jenkins.sh
@@ -35,6 +35,9 @@ run_test src/chinadns -V
 
 run_test tests/test.py -a '-c chnroute.txt -l iplist.txt' -t tests/google.com
 run_test tests/test.py -a '-c chnroute.txt -l iplist.txt' -t tests/facebook.com
+run_test tests/test.py -a '-c chnroute.txt -s 114.114.114.114,8.8.8.8 -l iplist.txt' -t tests/google.com
+run_test tests/test.py -a '-c chnroute.txt -y 0.5 -l iplist.txt' -t tests/google.com
+run_test tests/test.py -a '-c chnroute.txt -b 0.0.0.0 -l iplist.txt' -t tests/google.com
 
 run_test tests/test.py -a '-c chnroute.txt -l iplist.txt' -t tests/twitter.com
 run_test tests/test.py -a '-d -c chnroute.txt -l iplist.txt' -t tests/twitter.com
